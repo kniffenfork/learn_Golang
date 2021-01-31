87eb34100432c90b2ba64c8e71cefbb48982b01c
kniffenfork <fomin.as@phystech.edu>
2021-01-31 15:24:57 +0300

    added arguments of command line

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
+	numOfRoutines, _ := strconv.Atoi(os.Args[1])
+	iterCount, _ := strconv.Atoi(os.Args[2])
+	sizeArray, _ := strconv.Atoi(os.Args[3])
 	fmt.Println("Starting the dispatcher")
-	startDispatcher(5, 5, 100)
+	startDispatcher(numOfRoutines, iterCount, sizeArray)
 	time.Sleep(10 * time.Second)
 }
