31501ce6324cc32c30e9ca1e0650ac221e039f27
kniffenfork <fomin.as@phystech.edu>
2021-02-01 23:23:36 +0300

    parsing errors

--- main.go
+++ main.go
@@ -12,19 +12,24 @@ func main() {
 	numOfRoutines, err1 := strconv.Atoi(os.Args[1])
 	if err1 != nil {
 		fmt.Println("Error parsing number of routines")
+		os.Exit(1)
 	}
 
 	iterCount, err2 := strconv.Atoi(os.Args[2])
 	if err2 != nil {
 		fmt.Println("Error parsing iteration number")
+		os.Exit(1)
 	}
 
 	sizeArray, err3 := strconv.Atoi(os.Args[3])
 	if err3 != nil {
 		fmt.Println("Error parsing size of array")
+		os.Exit(1)
 	}
 
 	fmt.Println("Starting the dispatcher")
+
 	startDispatcher(numOfRoutines, iterCount, sizeArray)
+
 	time.Sleep(10 * time.Second)
 }
